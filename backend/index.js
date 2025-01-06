5fdb5545143f2538387708a85d8191aa2f66fb7f
naman <namangaur4102@gmail.com>
2025-01-06 12:48:33 +0530

    added router code

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const rootRouter = require("./routes/index");
 
+const app = express();
+app.use(cors());
+app.use(express.json());
 
+app.use("/api/v1", rootRouter);
+
+app.listen(3000, () => {
+    console.log("Server is listening at 3000")
+})
